09dcb93002480650c9f164c30585f4b11edda7e6
liqi <liqi@qq.com>
2017-11-16 10:59:35 +0800

    Custom unknown error message
    Custom unknown error message

--- nohttputils-r2/src/main/java/com/liqi/nohttputils/interfa/OnRequestRxNoHttpListener.java
+++
@@ -1,9 +0,0 @@
-package com.liqi.nohttputils.interfa;
-
-/**网络请求对外暴露接口
- * Created by LiQi on 2017/9/12.
- */
-
-public interface OnRequestRxNoHttpListener {
-    void requestRxNoHttp();
-}
--- nohttputils-r2/src/main/java/com/liqi/nohttputils/nohttp/rx_poll/operators/OnObserverEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.liqi.nohttputils.nohttp.rx_poll.operators;
-
-/**被观察者行动事件
- * Created by LiQi on 2017/9/6.
- */
-
-public interface OnObserverEventListener<V,T> {
-    T onObserverEvent(V transferValue);
-}
